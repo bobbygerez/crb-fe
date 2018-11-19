add3dab8de3411e8517bd4626fd00092a2b40299
unknown <bobby.gerez@yahoo.com>
2018-11-19 10:40:00 +0800

    Added Companies Module in Store

---
+++ src/store/companies/actions.js
@@ -0,0 +1,3 @@
+export const companies = (state, payload) => {
+  state.commit('companies', payload)
+}
---
+++ src/store/companies/mutations.js
@@ -0,0 +1,3 @@
+export const companies = (state, payload) => { state.companies = payload }
+
+export const company = (state, payload) => { state.company = payload }
