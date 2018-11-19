a32704f321b5b3ff6be97a367da41f73bd369cf7
unknown <bobby.gerez@yahoo.com>
2018-11-19 17:13:26 +0800

    Companies With Server Pagination

--- src/components/data-table/companies.vue
+++ src/components/data-table/companies.vue
@@ -3,27 +3,40 @@
     <q-table
       ref="table"
       color="primary"
-      title="Server-side Pagination"
+      title="All Companies"
       :data="serverData"
       :columns="columns"
       :filter="filter"
       row-key="name"
       :pagination.sync="serverPagination"
+      :rows-per-page-options="options"
       @request="request"
       :loading="loading"
     >
       <template slot="top-right" slot-scope="props">
         <q-search hide-underline v-model="filter" />
       </template>
+
+      <div slot="pagination" slot-scope="props" class="row flex-center q-py-sm">
+      <q-btn round dense size="sm" icon="undo" color="secondary" class="q-mr-sm" :disable="props.isFirstPage" @click="props.prevPage" />
+      <div class="q-mr-sm" style="font-size: small">
+        Page {{ props.pagination.page }} / {{ props.pagination.pagesNumber }}
+      </div>
+      <q-btn round dense size="sm" icon="redo" color="secondary" :disable="paginationLast(props.pagination.page)" @click="props.nextPage" />
+    </div>
+
     </q-table>
   </div>
 </template>
 
 <script>
 import tableData from 'assets/table-data'
+import _ from 'lodash'
 export default {
   data () {
     return {
+      options: [0],
+      lastPage: '',
       serverData: [],
       serverPagination: {
         page: 1,
@@ -31,20 +44,16 @@ export default {
       },
       columns: [
         {
-          name: 'desc',
+          name: 'name',
           required: true,
-          label: 'Dessert (100g serving)',
+          label: 'Name',
           align: 'left',
           field: 'name',
-          sortable: true
         },
-        { name: 'calories', label: 'Calories', field: 'calories', sortable: true },
-        { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
-        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-        { name: 'protein', label: 'Protein (g)', field: 'protein' },
-        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-        { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-        { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+        { name: 'desc', label: 'Description', field: 'desc',  align: 'left'},
+        { name: 'fat', label: 'Fat (g)', field: 'fat' },
+        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' }
+       
       ],
       filter: '',
       loading: false
@@ -59,51 +68,34 @@ export default {
 			}
 		},
   methods: {
+    paginationLast(currentPage){
+      if(this.lastPage > currentPage ){
+        return false
+      }
+      return true
+    },
     request (props) {
       this.loading = true
       let data = this
-      this.$axios.get(process.env.API + `/companies?page=${this.page}&perPage=${this.perPage}`)
-				.then(function (res) {
-          // data.$store.dispatch('companies', res.data.companies)
-          console.log(res.data.companies.data)
-          data.serverPagination =  props.pagination
-          let
-          table = data.$refs.table,
-          // rows = tableData.slice(),
-          rows = res.data.companies.data,
-          { page, rowsPerPage, sortBy, descending } = props.pagination
-
-          data.serverPagination.rowsNumber = res.data.companies.data.length
-          if (rowsPerPage) {
-            rows = rows.slice((page - 1) * rowsPerPage, page * rowsPerPage)
-          }
-          data.serverData = rows
-          data.loading = false
-          console.log(data.serverData)
-        })
-        
-      setTimeout(() => {
-        // this.serverPagination = props.pagination
-        // let
-        //   table = this.$refs.table,
-        //   rows = tableData.slice(),
-        //   { page, rowsPerPage, sortBy, descending } = props.pagination
-        
-        // console.log(rows)
-        // if (props.filter) {
-        //   rows = table.filterMethod(rows, props.filter)
-        // }
-        // if (sortBy) {
-        //   rows = table.sortMethod(rows, sortBy, descending)
-        // }
-
-        // this.serverPagination.rowsNumber = rows.length
-        // if (rowsPerPage) {
-        //   rows = rows.slice((page - 1) * rowsPerPage, page * rowsPerPage)
-        // }
-        // this.serverData = rows
-        // this.loading = false
-      }, 1500)
+      this.$axios
+            .get(process.env.API + `/companies?page=${props.pagination.page}&perPage=10`)
+            .then(function(res){
+              //I cannot disable the props.isLastPage
+              data.serverPagination = props.pagination
+              console.log(data.serverPagination)
+              data.serverData = _.values(res.data.companies.data)
+               data.serverPagination.rowsNumber = res.data.companies.total
+               data.lastPage = res.data.companies.last_page
+               data.loading = false
+              
+            })
+            
+            .catch(error => {
+                // there's an error... do SOMETHING
+                console.log(error)
+                // we tell QTable to exit the "loading" state
+                this.loading = false
+            })
 
     }
   },
