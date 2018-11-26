3bc7542e255adf61732e030e14d1d4641c3fc1cf
renren1 <renren@ixbase.net>
2018-11-26 13:26:12 +0800

    some fixes

--- src/components/mixins/countries.js
+++ src/components/mixins/countries.js
@@ -1,11 +1,8 @@
 export default {
-
   created () {
     let data = this
-    this.$axios.get(process.env.API + '/countries')
-      .then(function (res) {
-        data.$store.dispatch('countries', res.data.countries)
-      })
+    this.$axios.get('/countries').then(function (res) {
+      data.$store.dispatch('countries', res.data.countries)
+    })
   }
-
 }
