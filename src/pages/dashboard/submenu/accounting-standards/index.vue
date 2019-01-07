57f37240beba0ed318b9f8d6ff17d78a62292c4b
unknown <bobby.gerez@yahoo.com>
2019-01-07 17:16:55 +0800

    Updates

---
+++ src/pages/dashboard/submenu/accounting-standards/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="q-pa-sm">
+    <accounting-standards></accounting-standards>
+    <q-page-sticky
+      position="bottom-left"
+      :offset="[16, 16]"
+    >
+      <q-btn
+        color="primary"
+        @click="newAccountingStandard()"
+      >
+        <q-icon name="add"></q-icon>New Accounting Standard
+      </q-btn>
+    </q-page-sticky>
+  </div>
+</template>
+
+<script type="text/javascript">
+import accountingStandards from 'components/data-table/accounting-standards.vue'
+export default {
+
+  methods: {
+    
+    newAccountingStandard () {
+      this.$store.dispatch('accountingStandards/newAccountingStandard', true)
+      this.$store.dispatch('accountingStandards/accountingStandard', {
+        name: '',
+        mask: ''
+      })
+    }
+  },
+  components: {
+    accountingStandards
+  }
+}
+</script>
