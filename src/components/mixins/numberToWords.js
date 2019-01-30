d96a3f7abca7b6a45ae630bf323a18a0346a0cad
bobbygerez <bobby.gerez@yahoo.com>
2019-01-30 17:13:21 +0800

    Updates

--- src/components/mixins/numberToWords.js
+++ src/components/mixins/numberToWords.js
@@ -2,7 +2,6 @@ export default {
 
   methods: {
 
-<<<<<<< HEAD
       convertNumberToWords(amount) {
           var words = new Array();
           words[0] = '';
@@ -81,58 +80,6 @@ export default {
                   }
               }
               words_string = words_string.split("  ").join(" ");
-=======
-    convertNumberToWords (amount) {
-      var words = []
-      words[0] = ''
-      words[1] = 'One'
-      words[2] = 'Two'
-      words[3] = 'Three'
-      words[4] = 'Four'
-      words[5] = 'Five'
-      words[6] = 'Six'
-      words[7] = 'Seven'
-      words[8] = 'Eight'
-      words[9] = 'Nine'
-      words[10] = 'Ten'
-      words[11] = 'Eleven'
-      words[12] = 'Twelve'
-      words[13] = 'Thirteen'
-      words[14] = 'Fourteen'
-      words[15] = 'Fifteen'
-      words[16] = 'Sixteen'
-      words[17] = 'Seventeen'
-      words[18] = 'Eighteen'
-      words[19] = 'Nineteen'
-      words[20] = 'Twenty'
-      words[30] = 'Thirty'
-      words[40] = 'Forty'
-      words[50] = 'Fifty'
-      words[60] = 'Sixty'
-      words[70] = 'Seventy'
-      words[80] = 'Eighty'
-      words[90] = 'Ninety'
-      amount = amount.toString()
-      var atemp = amount.split('.')
-      var number = atemp[0].split(',').join('')
-      var nLength = number.length
-      var wordsString = ''
-      if (nLength <= 9) {
-        var nArray = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        var receivedNarray = []
-        for (let i = 0; i < nLength; i++) {
-          receivedNarray[i] = number.substr(i, 1)
-        }
-        for (let i = 9 - nLength, j = 0; i < 9; i++, j++) {
-          nArray[i] = receivedNarray[j]
-        }
-        for (let i = 0, j = 1; i < 9; i++, j++) {
-          if (i === 0 || i === 2 || i === 4 || i === 7) {
-            if (nArray[i] === 1) {
-              nArray[j] = 10 + parseInt(nArray[j])
-              nArray[i] = 0
-            }
->>>>>>> 89f5b4dd4a678ca2bf35ffdc7253e91b30ba2dba
           }
           return words_string;
       },
--- src/pages/dashboard/submenu/transactions/add.vue
+++ src/pages/dashboard/submenu/transactions/add.vue
@@ -7,15 +7,11 @@
         <div class="col-xs-3">
             <q-select v-model="transaction.transaction_type_id" :options="transactionTypes" float-label="Transaction Type" clearable />
             <q-select v-model="transaction.chart_account_id" filter :options="chartAccounts" float-label="GL account" clearable />
-            <input-price label="Vatable Sales" :value="vatable" v-model="vatable"></input-price>
-            <input-price label="Zero Rated Sales" :value="zeroRated" v-model="zeroRated"></input-price>
             <q-input v-model="transaction.remarks" type="textarea" float-label="Remarks" :max-height="100" rows="2" hide-underline />
         </div>
         <div class="col-xs-3">
             <input-price label="Total Amount" :value="transaction.total_amount" v-model="transaction.total_amount" class="q-ml-sm" :disabled="true"></input-price>
             <q-input v-model="createdBy" float-label="Created By" disable class="q-ml-sm" />
-            <input-price label="VAT-Exempt Sales" :value="vatExempt" v-model="vatExempt"></input-price>
-            <input-price label="VAT Amount" :value="vatAmount" v-model="vatAmount"></input-price>
         </div>
         <div class="col-xs-6" v-if="transactionType.taccount_id != 3">
             <div class="row">
@@ -48,9 +44,8 @@
         </div>
     </div>
 
-<<<<<<< HEAD
-    <div class="row" v-for="(item, index) in purchasesItems.items" :key="index">
-        <div class="col-xs-3">
+    <div class="row" v-for="(item) in purchasesItems.items" :key="item.id">
+        <div class="col-xs-2">
             <q-select v-model="item.id" filter :options="entityItems" float-label="Item Name" clearable class="q-ml-sm" />
         </div>
 
@@ -61,94 +56,37 @@
             <q-select v-model="item.chart_account_id" filter :options="chartAccounts" float-label="GL account" clearable />
         </div>
         <div class="col-xs-1">
-            <q-input :value="item.tax_type.name" float-label="Tax Type" disable />
+            <q-input :value="item.pivot.qty" float-label="Qty" disable />
         </div>
-
         <div class="col-xs-1">
             <negative-price label="Price" :value="item.pivot_price" v-model="item.pivot_price" :disabled="true"></negative-price>
         </div>
-        <div class="col-xs-1">
+        <div class="col-xs-2">
             <negative-price label="Debit Amount" :value="item.total_amount" v-model="item.total_amount" :disabled="true"></negative-price>
         </div>
-        <div class="col-xs-1">
-            <div class="col-xs-1">
-                <negative-price label="Input Tax" :value="item.purchase_tax" v-model="item.purchase_tax" :disabled="true"></negative-price>
-            </div>
-        </div>
 
-=======
-    <div
-      class="row"
-      v-for="(item) in purchasesItems.items"
-      :key="item.id"
-    >
-      <div class="col-xs-2">
-        <q-input
-          float-label="Item"
-          :value="item.name"
-          disable
-        />
-      </div>
-      <div class="col-xs-1">
-        <q-input
-          float-label="Qty"
-          :value="item.pivot.qty"
-          disable
-        />
-      </div>
-      <div class="col-xs-3">
-        <q-input
-          :value="item.desc"
-          float-label="Description"
-          disable
-        />
-      </div>
-      <div class="col-xs-3">
-        <q-input
-          :value="item.chart_account.name"
-          float-label="Chart Account"
-          disable
-        />
-      </div>
-
-      <div class="col-xs-2">
-        <negative-price
-          label="Debit Amount"
-          :value="item.pivot_price"
-          v-model="item.pivot_price"
-          :disabled="true"
-        ></negative-price>
-      </div>
-      <div class="col-xs-1">
-        <negative-price
-          label="Tax"
-          :value="waiting"
-          v-model="waiting"
-          :disabled="true"
-        ></negative-price>
-      </div>
->>>>>>> 89f5b4dd4a678ca2bf35ffdc7253e91b30ba2dba
     </div>
     <span v-if="selectedPurchase == null">
      <div class="row" v-for="(gl, index) in generalLedgers" :key="index" >
         <div class="col-xs-2" v-if="transactionType.taccount_id !== 3">
             <q-select v-model="gl.item_id" filter :options="entityItems" float-label="Item" class="q-ml-sm"/>
         </div>
-        
+        <div class="col-xs-1" v-if="transactionType.taccount_id !== 3">
+            <q-input float-label="Qty" value=""/>
+        </div>
         <div :class="transactionType.taccount_id != 3 ? 'col-xs-3' : 'col-xs-5'">
             <q-input v-model="gl.particulars" float-label="Particulars" />
         </div>
         <div class="col-xs-3">
             <q-select v-model="gl.chart_account_id" filter :options="chartAccounts" float-label="GL account" clearable />
         </div>
+
         <div class="col-xs-2" v-if="transactionType.taccount_id === 2 || transactionType.taccount_id === 3">
             <negative-price label="Debit Amount" :value="gl.debit_amount" v-model="gl.debit_amount"></negative-price>
         </div>
-        <div class="col-xs-1" v-if="transactionType.taccount_id !== 3">
-            <q-input float-label="Qty" value=""/>
-        </div>
-        <div class="col-xs-1">
-            <negative-price label="Price" :value="gl.price" v-model="gl.price" ></negative-price>
+        <div class="col-xs-1" v-if="transactionType.taccount_id != 3">
+             <q-btn color="primary" size="sm" icon="close" flat round class="float-right" @click="removeGl(index)" v-if="transactionType.taccount_id === 2" />
+            <input-price label="Tax" :value="gl.tax" v-model="gl.tax"></input-price>
         </div>
         <div class="col-xs-2" v-if="transactionType.taccount_id === 1 || transactionType.taccount_id === 3">
             <q-btn color="primary" size="sm" icon="close" flat round class="float-right" @click="removeGl(index)" />
@@ -178,10 +116,6 @@ export default {
     mixins: [numberToWords],
     data() {
         return {
-            vatAmount: 0,
-            vatable: 0,
-            vatExempt: 0,
-            zeroRated: 0,
             selectedItem: '',
             purchasesItems: [],
             selectedPurchase: null,
@@ -193,11 +127,9 @@ export default {
             credit_amount: 0,
             tax: 0,
             generalLedgers: [],
-            oldGeneralLedgers: [],
             selectedChartAccount: 0
         }
     },
-<<<<<<< HEAD
     computed: {
         ...mapState('transactions', ['company', 'transaction', 'selectedEntity', 'selectedUserEntity', 'payee', 'transactionType', 'entities']),
         entityItems() {
@@ -255,15 +187,6 @@ export default {
         },
         createdBy() {
             return `${this.transaction.created_by.firstname} ${this.transaction.created_by.lastname}`
-=======
-    purchases () {
-      return this.$store.getters['transactions/purchases'].map(e => {
-        let invoice = e.invoice_no
-        return {
-          label: `${invoice.substring(1, 21)} (Invoice No.) `,
-          value: e.id,
-          items: e.items
->>>>>>> 89f5b4dd4a678ca2bf35ffdc7253e91b30ba2dba
         }
     },
     methods: {
@@ -300,26 +223,16 @@ export default {
             }
         },
         addGl() {
-            let id = 1
-            if (this.generalLedgers.length > 0) {
-                id = _.maxBy(this.generalLedgers, function (o) {
-                    return o.id;
-                }).id + 1;
-            }
             this.generalLedgers.push({
                 ledgerable_id: this.selectedUserEntity,
                 ledgerable_type: this.selectedEntity,
-                id: id,
-                item_id: 0,
+                id: '',
                 particulars: '',
                 chart_account_id: '',
                 debit_amount: 0,
-                price: 0,
                 tax: 0,
                 credit_amount: 0
             })
-
-            this.setValue();
         },
         getTransactionTypes() {
             this.$axios.get(`transactions/create?modelType=${this.selectedEntity}&modelId=${this.selectedUserEntity}`)
@@ -333,36 +246,19 @@ export default {
                 })
         },
         store() {
-
-            if (this.transactionType === 3) {
+            if (this.transactionType.taccount_id === 3) {
                 if (this.debit_amount != this.credit_amount) {
                     this.$q.notify({
                         color: 'negative',
                         icon: 'warning',
                         message: `Debit and Credit amount must be equal.`
                     })
-<<<<<<< HEAD
                 } else {
-                    // this.createTransaction()
+                    this.createTransaction()
                 }
             } else {
-
-                this.generalLedgers = this.purchasesItems.items.map(e => {
-                    return {
-                        ledgerable_id: this.selectedUserEntity,
-                        ledgerable_type: this.selectedEntity,
-                        item_id: e.id,
-                        qty: e.qty,
-                        particulars: e.name,
-                        chart_account_id: e.chart_account_id,
-                        debit_amount: e.total_amount,
-                    }
-
-                })
                 this.createTransaction()
-
             }
-
         },
         createTransaction() {
             this.$axios
@@ -376,27 +272,17 @@ export default {
                         checknumber: this.transaction.checknumber,
                         remarks: this.transaction.remarks
                     },
-                    generalLedgers: this.generalLedgers,
-                    payee: {
-                        vendorableName: this.selectedVendorableName,
-                        vendorableType: this.selectedVendorableType
-                    }
+                    generalLedgers: this.generalLedgers
                 })
                 .then(res => {
-=======
-                  })
-                  .catch((err) => {
->>>>>>> 89f5b4dd4a678ca2bf35ffdc7253e91b30ba2dba
                     this.$q.notify({
                         color: 'positive',
                         icon: 'check',
                         message: `This transaction has been updated successfully`
                     })
-<<<<<<< HEAD
                     this.generalLedgers = []
                     this.addGl()
                     this.intialize()
-
                 })
                 .catch()
         },
@@ -415,88 +301,6 @@ export default {
             this.tax = tax
             //Disbursement Selected Normal balance is debit
             //contra account credit
-=======
-                  })
-              }
-            }
-          ]
-        })
-      } else {
-        this.generalLedgers.splice(index, 1)
-      }
-    },
-    addGl () {
-      this.generalLedgers.push({
-        ledgerable_id: this.selectedUserEntity,
-        ledgerable_type: this.selectedEntity,
-        id: '',
-        particulars: '',
-        chart_account_id: '',
-        debit_amount: 0,
-        tax: 0,
-        credit_amount: 0
-      })
-    },
-    getTransactionTypes () {
-      this.$axios.get(`transactions/create?modelType=${this.selectedEntity}&modelId=${this.selectedUserEntity}`)
-        .then(res => {
-          this.$store.dispatch('transactions/transactionTypes', res.data.transactionTypes)
-          this.$store.dispatch('transactions/chartAccounts', res.data.chartAccounts)
-          this.$store.dispatch('transactions/createdBy', res.data.createdBy)
-          // this.$store.dispatch('transactions/transaction', res.data.transaction)
-          // this.generalLedgers = res.data.transaction.general_ledgers
-          // this.$store.dispatch('transactions/payee', res.data.payee)
-        })
-    },
-    store () {
-      if (this.transactionType.taccount_id === 3) {
-        if (this.debit_amount !== this.credit_amount) {
-          this.$q.notify({
-            color: 'negative',
-            icon: 'warning',
-            message: `Debit and Credit amount must be equal.`
-          })
-        } else {
-          this.createTransaction()
-        }
-      } else {
-        this.createTransaction()
-      }
-    },
-    createTransaction () {
-      this.$axios
-        .post(`/transactions`, {
-          transaction: {
-            transactable_id: this.selectedUserEntity,
-            transactable_type: this.selectedEntity,
-            transaction_type_id: this.transaction.transaction_type_id,
-            chart_account_id: this.transaction.chart_account_id,
-            total_amount: this.transaction.total_amount,
-            checknumber: this.transaction.checknumber,
-            remarks: this.transaction.remarks
-          },
-          generalLedgers: this.generalLedgers
-        })
-        .then(res => {
-          this.$q.notify({
-            color: 'positive',
-            icon: 'check',
-            message: `This transaction has been updated successfully`
-          })
-          this.generalLedgers = []
-          this.addGl()
-          this.intialize()
-        })
-        .catch()
-    },
-    totalAmount (generalLedgers) {
-      /* eslint-disable */
-      let debit_amount = _.sumBy(generalLedgers, function (i) {
-        return i.debit_amount
-      })
-      this.debit_amount = debit_amount
->>>>>>> 89f5b4dd4a678ca2bf35ffdc7253e91b30ba2dba
-
             if (this.transactionType.taccount_id === 1) {
                 let total_amount = parseFloat(credit_amount) + parseFloat(tax);
                 this.numToWords = this.withDecimal(total_amount)
@@ -517,29 +321,22 @@ export default {
                     this.numToWords = ''
                     this.$store.dispatch('transactions/transactionTotalAmount', 0)
                 }
-
             }
         },
         intialize() {
-
             this.$store.dispatch('transactions/chartAccountId', 0)
             this.$store.dispatch('transactions/transactionRemarks', '');
-        },
-        setValue: function () {
-            this.$data.oldGeneralLedgers = _.cloneDeep(this.$data.generalLedgers);
         }
     },
     mounted() {
         this.getTransactionTypes()
         this.intialize()
         this.addGl()
-        this.setValue();
     },
     components: {
         inputPrice,
         negativePrice
     },
-<<<<<<< HEAD
     watch: {
         'transaction.chart_account_id'(val) {
             this.$store.dispatch('transactions/chartAccountId', val)
@@ -562,32 +359,6 @@ export default {
         },
         generalLedgers: {
             handler: function (after, before) {
-                var vm = this;
-                let changed = after.filter(function (p, idx) {
-                    return Object.keys(p).some(function (prop) {
-                        return p[prop] !== vm.$data.oldGeneralLedgers[idx][prop];
-                    })
-                })
-                // Log it
-
-                if (changed.length === 1) {
-                    var item = {}
-                    this.$axios.get(`/items/${_.head(changed).id}?id=${_.head(changed).id}`)
-                        .then(function (res){
-                            item = res.data.item
-                            
-                            Object.keys(vm.generalLedgers).forEach(function (key) {
-                                if (vm.generalLedgers[key].id === _.head(changed).id) {
-                                    vm.generalLedgers[key].particulars = item.desc
-                                    vm.generalLedgers[key].chart_account_id = item.chart_account_id
-                                    vm.generalLedgers[key].price = item.price
-                                }
-                            });
-                        })
-
-                }
-                vm.setValue();
-
                 this.totalAmount(after)
             },
             deep: true,
@@ -613,85 +384,18 @@ export default {
         },
         'selectedPurchase'(val) {
             if (val !== null) {
-                this.purchasesItems = _.find(this.purchases, (x) => {
+                this.purchasesItems = _.find(this.purchases, function (x) {
                     return x.value === val
                 })
-
-                let total_amount = _.sumBy(this.purchasesItems.items, (i) => {
+                let total_amount = _.sumBy(this.purchasesItems.items, function (i) {
                     return i.total_amount;
                 })
-
-                let vatableItems = _.filter(this.purchasesItems.items, (i) => {
-                    return i.tax_type_id === 1
-                })
-                this.vatable = _.sumBy(vatableItems, (i) => {
-                    return i.total_amount;
-                })
-                this.vatAmount = _.sumBy(vatableItems, (i) => {
-                    return i.purchase_tax;
-                })
-
-                let vatExemptItems = _.filter(this.purchasesItems.items, (i) => {
-                    return i.tax_type_id === 2
-                })
-                this.vatExempt = _.sumBy(vatExemptItems, (i) => {
-                    return i.total_amount;
-                })
-
-                let zeroRated = _.filter(this.purchasesItems.items, (i) => {
-                    return i.tax_type_id === 3
-                })
-                this.zeroRated = _.sumBy(zeroRated, (i) => {
-                    return i.total_amount;
-                })
-
                 this.$store.dispatch('transactions/transactionTotalAmount', total_amount)
                 this.numToWords = this.withDecimal(total_amount)
             } else {
                 this.purchasesItems = []
             }
         }
-=======
-    'transaction.remarks' (val) {
-      this.$store.dispatch('transactions/transactionRemarks', val)
-    },
-    'transaction.checknumber' (val) {
-      this.$store.dispatch('transactions/transactionCheckNumber', val)
-    },
-    generalLedgers: {
-      handler: function (after, before) {
-        this.totalAmount(after)
-      },
-      deep: true
-    },
-    'selectedVendorableType' (val) {
-      if (val !== '') {
-        this.$axios
-          .get(
-            `/transactions-entities?modelType=${val}`
-          )
-          .then(res => {
-            this.$store.dispatch('transactions/vendorableNames', res.data.userEntities)
-            // this.$store.dispatch('transactions/selectedUserEntity', '')
-          })
-      }
-    },
-    'selectedVendorableName' (val) {
-      this.$axios.get(`transactions-get-purchases?modelType=${this.selectedVendorableType}&modelId=${val}`)
-        .then(res => {
-          this.$store.dispatch('transactions/purchases', res.data.purchases)
-          this.$store.dispatch('transactions/entityItems', res.data.entityItems)
-        })
-    },
-    'selectedPurchase' (val) {
-      if (val !== null) {
-        this.purchasesItems = _.find(this.purchases, function (x) {
-          return x.value === val
-        })
-      } else {
-        this.purchasesItems = []
-      }
->>>>>>> 89f5b4dd4a678ca2bf35ffdc7253e91b30ba2dba
     }
 }
 </script>
